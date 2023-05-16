adb1f39d7459ee741b5fc8573a35277220a65024
Leku <lekhanagowda20@gmail.com>
2023-05-16 16:27:39 +0530

    added post liking functionaity

--- src/app/blog-details/blog-details.component.ts
+++ src/app/blog-details/blog-details.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BlogService } from '../_services/blog.service';
+import { PostsService } from '../_services/posts.service';
+import { UserService } from '../_services';
 
 interface Blog {
   name: string;
@@ -9,6 +11,8 @@ interface Blog {
   tags: string[];
   date: string;
   username: string;
+  _id?: string;
+  likes?: []
 }
 
 @Component({
@@ -17,12 +21,12 @@ interface Blog {
   styleUrls: ['./blog-details.component.scss'],
 })
 export class BlogDetailsComponent {
-  constructor(private routes: ActivatedRoute, private blogSvc: BlogService) {
+  constructor(private routes: ActivatedRoute, private blogSvc: BlogService, private postService: PostsService, private authService: UserService) {
     this.routes.paramMap.subscribe((map: any) => {
       console.log('map:', map);
       this.blogID = map?.params?.id || null;
     });
-
+    this.getCurrentUser();
     this.getBlogDetails();
   }
 
@@ -41,4 +45,25 @@ export class BlogDetailsComponent {
     date: '',
     username: '',
   };
+
+  user
+  subs = []
+
+  like(_id: string) {
+    // const userLiked = currPost.likes.some((like) => like.user === this.user.id);
+    const userLiked = this.blog_props.likes.some((like: any) => like.user === this.user.id)
+    if (!userLiked)
+      this.postService.postLike(_id).subscribe((response) => {
+        this.getBlogDetails()
+      });
+  }
+
+  getCurrentUser() {
+    this.subs.push(
+      this.authService.getCurrentUser().subscribe((user) => {
+        this.user = user;
+        this.user.av = `https://api.dicebear.com/6.x/initials/svg?seed=${user.name}`;
+      })
+    );
+  }
 }
